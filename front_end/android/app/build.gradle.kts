631b4bca973b1af3f1b0b9424b4626eb05d298da
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-09-06 23:23:53 -0500

    config: update sdk versions to match

--- front_end/android/app/build.gradle.kts
+++ front_end/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.penny"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
